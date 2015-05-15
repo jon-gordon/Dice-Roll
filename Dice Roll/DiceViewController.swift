79beab420a2977f7f50a70b7883ac0784947df68
Jon Gordon <jon@jongor-software.com>
2015-05-15 21:29:37 +0100

    Now rolls the dice!

--- Dice Roll/DiceViewController.swift
+++ Dice Roll/DiceViewController.swift
@@ -16,9 +16,31 @@ class DiceViewController: UIViewController {
     @IBOutlet weak var firstDie: UIImageView!
     @IBOutlet weak var secondDie: UIImageView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    override func viewWillAppear(animated: Bool) {
+
+        // The dice will only appear if firstValue and secondValue have been set
+        if let firstValue = self.firstValue {
+            self.firstDie.image = UIImage(named: "d\(firstValue)")
+        } else {
+            self.firstDie.image = nil;
+        }
+
+        if let secondValue = self.secondValue {
+            self.secondDie.image = UIImage(named: "d\(secondValue)")
+        } else {
+            self.secondDie.image = nil
+        }
+
+        self.firstDie.alpha = 0
+        self.secondDie.alpha = 0
+    }
+
+    override func viewDidAppear(animated: Bool) {
+
+        UIView.animateWithDuration(0.3) {
+            self.firstDie.alpha = 1
+            self.secondDie.alpha = 1
+        }
     }
 
     func getImageFromValue(value: Int?) -> UIImage? {
